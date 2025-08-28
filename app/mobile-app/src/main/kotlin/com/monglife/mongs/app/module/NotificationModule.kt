62106115d59f59d4ade843e4d3e0670c91605ec6
tableminpark <tablemin_park@daum.net>
2025-08-28 20:36:31 +0900

    fix: 모바일 앱 설정

---
+++ app/mobile-app/src/main/kotlin/com/monglife/mongs/app/module/NotificationModule.kt
@@ -0,0 +1,43 @@
+package com.monglife.mongs.app.module
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import com.google.firebase.messaging.FirebaseMessaging
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NotificationModule {
+
+    const val CHANNEL_ID = "mongs_channel"
+    private const val CHANNEL_NAME = "mongs notification channel"
+
+    @Provides
+    @Singleton
+    fun provideFirebaseMessaging() : FirebaseMessaging {
+        return FirebaseMessaging.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNotificationManager(@ApplicationContext context: Context) : NotificationManager {
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        )
+
+        return notificationManager
+    }
+}
---
+++ app/mobile-app/src/main/kotlin/com/monglife/mongs/app/service/NotificationService.kt
@@ -0,0 +1,76 @@
+package com.monglife.mongs.app.service
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.monglife.mongs.app.activity.MainActivity
+import com.monglife.mongs.app.module.NotificationModule
+import com.monglife.mongs.application.auth.usecase.SyncUserDeviceUseCase
+import com.monglife.mongs.application.device.usecase.GetNotificationOptionUseCase
+import com.mongs.presentation.view.mobile.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class NotificationService : FirebaseMessagingService() {
+
+    @Inject lateinit var notificationManager: NotificationManager
+    @Inject lateinit var syncUserDeviceUseCase: SyncUserDeviceUseCase
+    @Inject lateinit var getNotificationOptionUseCase: GetNotificationOptionUseCase
+
+    override fun onNewToken(token: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            syncUserDeviceUseCase(
+                command = SyncUserDeviceUseCase.Command(
+                    fcmToken = token,
+                )
+            )
+        }
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (remoteMessage.data.isNotEmpty()) {
+                if (getNotificationOptionUseCase()) {
+                    sendNotification(
+                        title = remoteMessage.data["title"].toString(),
+                        body = remoteMessage.data["body"].toString(),
+                    )
+                }
+            }
+        }
+    }
+
+    @SuppressLint("WearRecents")
+    private fun sendNotification(title: String, body: String) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            System.currentTimeMillis().toInt(),
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+        )
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, NotificationModule.CHANNEL_ID)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(R.drawable.icon_logo_not_open)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(false)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
